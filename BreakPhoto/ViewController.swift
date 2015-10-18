373fadf47d485f6884108df4c7c2c2248380649c
Hla Min <1986hlamin@gmail.com>
2015-10-18 16:10:05 +0900

    add based project

---
+++ BreakPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BreakPhoto
+//
+//  Created by Hla Min on 10/18/15.
+//  Copyright © 2015 BigOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
